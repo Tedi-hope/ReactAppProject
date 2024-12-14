f5615990d78f3e4a15da8f413b24b497255a4c58
Tedros Habtu <amhopefule07@gmail.com>
2024-12-14 17:31:56 +0300

    Project is setup Ract is configured with tainwindcss

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  
+  return (
+    <div className="bg-blue-500 text-white text-center p-5 py-7">
+      <div className="flex flex-col justify-between md:flex-row space-y-10 space-x-0 md:space-x-10 md:space-y-0">
+       <h1 className="text-4xl font-bold uppercase">React With TailWindCSS</h1>
+      <h1 className="text-4xl font-bold uppercase">Hello, Tailwind CSS with Vite!</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App
